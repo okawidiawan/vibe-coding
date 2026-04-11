1c4e9ee4102206381b5aa84614d3610d365ce82b
Oka Widiawan <oka.widiawan@gmail.com>
2026-04-11 21:25:45 +0700

    feat: implement swagger ui documentation

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import { Elysia } from 'elysia';
 import { db } from '../db';
 import { users } from '../db/schema';
+import { swagger } from '@elysiajs/swagger';
 import { usersRoute } from './routes/users-route';
 
 export const app = new Elysia()
+  .use(swagger({
+    documentation: {
+      info: {
+        title: 'Vibe Coding User API',
+        version: '1.0.0',
+        description: 'API Documentation for User Management'
+      }
+    }
+  }))
   .use(usersRoute)
   .get('/', () => {
     return {
